b177ab80983d01edb967d72663cf9bb6755e6e1d
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-10 05:07:48 +1000

    added npc move
    Navmesh agent added

--- Assets/Scripts/NPCMove.cs
+++ Assets/Scripts/NPCMove.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPCMove : MonoBehaviour
 {
     //Variables go here
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
 
     // Start is called before the first frame update
     void Start()
     {
-        //This code in these brackets starts every tie game is started first    
+        //This code in these brackets starts every time game is started first    
     }
 
     // Update is called once per frame
