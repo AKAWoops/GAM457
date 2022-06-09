3e605d997666b3c7e50269370375c38892e62ae7
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-10 05:03:54 +1000

    copied new files

---
+++ Assets/Scripts/NPCMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMove : MonoBehaviour
+{
+    //Variables go here
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This code in these brackets starts every tie game is started first    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // in these brackets code is run every frame when game is running
+    }
+}
