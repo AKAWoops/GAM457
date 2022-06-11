2e38a9eebf9816472624aa6a2fb6a69bd26fb596
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-11 12:49:20 +1000

    Energy
    Working on the script to be external

--- Assets/Scripts/Energy.cs
+++ Assets/Scripts/Energy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,33 @@ public class Energy : MonoBehaviour
     private int Timer = 30;
     [SerializeField]
     public float EnergyAmount = 10;
+
+    private int totalEnergy = 0;
+
+    private DateTime nextEnergyTime;
+
+    private DateTime lastAddedTime;
+
+    private int EnergyRestoreDuration = 10; // 10 secconds for now
+
+
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void Load()
+    {
+
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetInt("TotalEnergy", totalEnergy);
+
+    }
     // Update is called once per frame
     void Update()
     {
