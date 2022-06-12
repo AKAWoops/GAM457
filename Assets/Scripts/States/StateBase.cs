3abf6cedef54111c771ece8f61cb4d0f41ddb6f3
Mark Ballinger <markballinger@optusnet.coml.au>
2022-06-12 20:31:21 +1000

    fucked the code start again

--- Assets/Scripts/States/StateBase.cs
+++ Assets/Scripts/States/StateBase.cs
@@ -3,62 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class StateBase : MonoBehaviour
+public abstract class StateBase
 {
-    public enum STATE
-    {
-        IDLE, PATROL, PURSUE, ATTACK, SLEEP, GATHERFOOD, EAT
-
-    };
-    public enum EVENT
-    {
-        ENTER, UPDATE, EXIT
-
-    };
-
-    public STATE name;
-    protected EVENT stage;
-    protected GameObject npc;
-    protected Animator anim;
-    protected Transform player;
-    protected STATE nextState;
-    protected NavMeshAgent agent;
-
-    float visDistance = 10.0f;
-    float visAngle = 30.0f;
-    float ShootDist = 7.0f;
-
-    public StateBase(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
-    {
-        npc = _npc;
-        agent = _agent;
-        anim = _anim;
-        stage = EVENT.ENTER;
-        player = _player;
-    }
-
-    public virtual void Enter() 
-    {
-        stage = EVENT.UPDATE;
-    }
-    public virtual void Update()
-    {
-        stage = EVENT.UPDATE;
-    }
-    public virtual void Exit()
-    {
-        stage = EVENT.EXIT;
-    }
    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
